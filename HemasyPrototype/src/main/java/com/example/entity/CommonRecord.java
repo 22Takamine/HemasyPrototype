b872bf101d860e18112b96c865b859fff7627ee3
amekuyasunari <22ameku_yasunari@axiz.co.jp>
2022-06-22 09:21:31 +0900

    CommonRecord追加

---
+++ HemasyPrototype/src/main/java/com/example/entity/CommonRecord.java
@@ -0,0 +1,45 @@
+package com.example.entity;
+
+import java.sql.Date;
+
+import lombok.Data;
+
+@Data
+public class CommonRecord {
+
+	private int id;
+	private int category;
+	private int type;
+	private String value1;
+	private Integer value2;
+	private Integer value3;
+	private Integer value4;
+	private Integer value5;
+	private Integer value6;
+	private Integer value7;
+	private Integer value8;
+	private Date createDate;
+	private int userId;
+	
+	
+	/*
+	 * public int getCategory() { return category; } public void setCategory(int
+	 * category) { this.category = category; } public int getType() { return type; }
+	 * public void setType(int type) { this.type = type; } public String getValue1()
+	 * { return value1; } public void setValue1(String value1) { this.value1 =
+	 * value1; } public double getValue2() { return value2; } public void
+	 * setValue2(Integer value2) { this.value2 = value2; } public double getValue3()
+	 * { return value3; } public void setValue3(Integer value3) { this.value3 =
+	 * value3; } public double getValue4() { return value4; } public void
+	 * setValue4(Integer value4) { this.value4 = value4; } public double getValue5()
+	 * { return value5; } public void setValue5(Integer value5) { this.value5 =
+	 * value5; } public double getValue6() { return value6; } public void
+	 * setValue6(Integer value6) { this.value6 = value6; } public double getValue7()
+	 * { return value7; } public void setValue7(Integer value7) { this.value7 =
+	 * value7; } public double getValue8() { return value8; } public void
+	 * setValue8(Integer value8) { this.value8 = value8; } public Date getDate() {
+	 * return createDate; } public void setDate(Date date) { this.createDate = date;
+	 * } public int getUserId() { return userId; } public void setUserId(int userId)
+	 * { this.userId = userId; }
+	 */
+}
