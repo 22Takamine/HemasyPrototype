d8ec4735cdda293dc63ab68bfaa831b4768b4699
Takamine Yusuke <22takamine_yuusuke@axiz.co.jp>
2022-06-23 12:36:59 +0900

    ei

--- HemasyPrototype/src/main/java/com/example/form/InformationForm.java
+++ HemasyPrototype/src/main/java/com/example/form/InformationForm.java
@@ -1,12 +1,32 @@
 package com.example.form;
 
-import lombok.Data;
-
-@Data
+//@Data
 public class InformationForm {
 
 	private int userId;
 	private String title;
 	private String contents;
+	
+	
+	public int getUserId() {
+		return userId;
+	}
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+	
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	
+	public String getContents() {
+		return contents;
+	}
+	public void setContents(String contents) {
+		this.contents = contents;
+	}
 	
 }
